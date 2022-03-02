73a70d9420b7cfb77848e4eb25c0830c1b0e405c
Alexandre Baron <baron.alx@gmail.com>
2022-03-02 15:06:49 -0500

    Player now rotates towards movemend direction

--- Platformer3D/Assets/Scripts/PlayerController.cs
+++ Platformer3D/Assets/Scripts/PlayerController.cs
@@ -6,8 +6,9 @@ public class PlayerController : MonoBehaviour
 {
 
     public float    MoveSpeed = 1;
+    public float    RotationSpeed = 1;
     public float    JumpForce = 1;
-    public float GravityScale = 5;
+    public float    GravityScale = 5;
 
     public CharacterController Controller;
 
@@ -15,7 +16,7 @@ public class PlayerController : MonoBehaviour
 
     private Vector3 MoveDirection = new Vector3();
 
-
+    public GameObject PlayerModel;
 
 
     // Start is called before the first frame update
@@ -46,6 +47,11 @@ public class PlayerController : MonoBehaviour
 
         // Only rotate the player if it's moving around
         if (MoveDirection.x != 0f || MoveDirection.z != 0f)
+        {
             transform.rotation = Quaternion.Euler(0f, PlayerCamera.transform.rotation.eulerAngles.y, 0f);
+
+            Quaternion newRotation = Quaternion.LookRotation(new Vector3(MoveDirection.x, 0f, MoveDirection.z));
+            PlayerModel.transform.rotation = Quaternion.Slerp(PlayerModel.transform.rotation, newRotation, RotationSpeed * Time.deltaTime);
+        }
     }
 }
